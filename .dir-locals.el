763fc12760cbbabb3dedede105c917831d90e4ca
Jonathan Appavoo <jappavoo@bu.edu>
2014-12-05 23:09:30 -0500

    This file ensures that when program source is opened in emacs it automatically is commited when saved
    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,14 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c-mode
-  . ((eval git-auto-commit-mode 1))))
+  . ((eval git-auto-commit-mode 1)))
+ (c++-mode
+  . ((eval git-auto-commit-mode 1)))
+ (makefile-mode
+  . ((eval git-auto-commit-mode 1)))
+ (emacs-lisp-mode
+  . ((eval git-auto-commit-mode 1))) 
+ (sh-mode
+  . ((eval git-auto-commit-mode 1))) 
+)
 
